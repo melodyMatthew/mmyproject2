c19ed86cbb1fa1a88e0190c5ce4aafeb687b692d
melodyMatthew <meloody,s96309@gmail.com>
2022-04-29 13:58:22 +0800

    5

--- src/main/java/com/mel/ticket/StudentTicket.java
+++ src/main/java/com/mel/ticket/StudentTicket.java
@@ -4,7 +4,7 @@ public class StudentTicket extends Ticket{
     float off = 0.1f;
     public StudentTicket(Station start,Station end){
         super(start,end);
-        price = price - (int)*(price*off);
+        price = price - (int)(price*off);
 
     }
 }
--- src/main/java/com/mel/ticket/Ticket.java
+++ src/main/java/com/mel/ticket/Ticket.java
@@ -1,37 +1,41 @@
 package com.mel.ticket;
 
 public class Ticket {
+   public static final int TAIPEI_ATATION = 100;
+   public static final int TAICHUNG_STATION = 200;
+   public static final int KAOSIUNG_STATION = 300;
+
+
    Station start;
    Station end;
    int price;
-   public Ticket(Station start,Station end){
+   public Ticket(Station start,Station end) {
       this.start = start;
       this.end = end;
 
-   if(start == Station.TAIPEI_STATION )
-   {if(end == Station.TAICHUNG_STATION){
-      price =100;
-   }else {
-      price = 200;
-   }
+      if (start == Station.TAIPEI_STATION) {
+         if (end == Station.TAICHUNG_STATION) {
+            price = 600;
+         } else {
+            price = 1500;
+         }
 
-   }else if (start == Station.TAICHUNG_STATION){
-      if(end== Station.TAIPEI_STATION){
-         price =300;
-      }else {
-         price = 400;
-      }
-   }else if(start ==Station.KAOHSIUNG_STATION){
-      if(end == Station.TAIPEI_STATION){
-         price =500;
-      }else {
-         price = 600;
+      } else if (start == Station.TAICHUNG_STATION) {
+         if (end == Station.TAIPEI_STATION) {
+            price = 600;
+         } else {
+            price = 900;
+         }
+      } else if (start == Station.KAOHSIUNG_STATION) {
+         if (end == Station.TAIPEI_STATION) {
+            price = 900;
+         } else {
+            price = 1500;
+         }
       }
    }
-
 
    public void print(){
       System.out.println(start.name +"\t"+end.name +"\t"+price);
-   }
 
 }}
